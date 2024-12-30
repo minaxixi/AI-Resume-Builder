6aede87f4d81766bde638e545849841eedfe18f4
Mina Zhou <mina.zhouxi@gmail.com>
2024-12-29 16:07:59 -0800

    Setup clean deployment configuration

--- frontend/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Replace environment variables in the built JS files
-find /usr/share/nginx/html -type f -name "*.js" -exec sed -i "s|REACT_APP_API_URL|${REACT_APP_API_URL}|g" {} +
-
-# Start nginx
-nginx -g 'daemon off;'
